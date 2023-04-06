67cc88e3cabb746285623fb8dd1b62450880202b
Yu Li <lyo.gavin@gmail.com>
2023-04-06 13:13:56 -0500

    finetuen

---
+++ MultichoiceTemplateALpacaLORAWAYDataset.py
@@ -0,0 +1,142 @@
+# coding=utf8
+import os
+import pytorch_lightning as pl
+#from torch.utils.data import DataLoader, Dataset
+from datasets import load_dataset
+from tqdm import tqdm
+from transformers import LlamaTokenizer
+import pandas as pd
+import torch
+from functools import partial
+
+def generate_and_tokenize_prompt(data_point, tokenizer=None, max_seq_length=100):
+    def get_promote_title_type(row):
+        return row['title_template_name']
+
+    truncated_title = data_point['title']
+    truncated_title = truncated_title[:120] if isinstance(truncated_title, str) else ' '
+
+    instruction_prompt_first = f"""Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+
+### Instruction:
+你是自媒体创作者，需要为输入的文案内容，撰写指定类型的爆款标题。需要撰写的标题类型：{get_promote_title_type(data_point)}。
+
+### Input:
+{data_point['content']}"""
+
+    instruction_prompt_second = f"""
+
+### Response:{truncated_title}
+"""
+
+    input_ids = tokenizer.encode((instruction_prompt_first, instruction_prompt_second),
+                                      truncation='only_first',
+                                      max_length=max_seq_length)
+
+    truncated_title_input_ids = tokenizer.encode(truncated_title)
+
+    target_len = len(truncated_title_input_ids)
+
+    # input_ids = torch.tensor([prefix_input_ids + postfix_input_ids])
+    # attention_mask = torch.tensor([[1] * (len(postfix_input_ids) + len(prefix_input_ids))])
+
+    # inputs_dict = self.tokenizer.encode_plus(item['prompted_content'],
+    #                                         max_length=self.max_seq_length, padding='max_length',
+    #                                         truncation=True, return_tensors='pt')
+    # prompt_inputs_dict = self.tokenizer.encode_plus(item['prompt'],
+    #                                         max_length=self.max_seq_length, padding=False,
+    #                                         truncation=True, return_tensors='pt')
+    target = input_ids
+    labels = target.clone().detach()
+    labels[target == tokenizer.pad_token_id] = -100
+    return {
+        "input_ids": input_ids.squeeze(),
+        "attention_mask": [1] * len(input_ids.squeeze()),  # attention_mask.squeeze(),
+        "labels": [-100] * (len(input_ids.squeeze()) - target_len) + truncated_title_input_ids,
+        # labels.squeeze(),
+        # "labels_mask": torch.tensor([[0] * len(prefix_input_ids) + [1] * len(postfix_input_ids)])
+        # "answer_end_pos":len(answer_input_ids) + len(postfix_input_ids) + len(prefix_input_ids) - 1
+    }
+
+
+if __name__ == '__main__':
+    import argparse
+    #modelfile = '/cognitive_comp/wuziwei/pretrained_model_hf/medical_v2'
+    datafile = '/home/ubuntu/cloudfs/ghost_data/merge_all_add_1208_1228//merge_all_0108_with_multichoice_scores_and_templates_val_sample_1673194850.csv.tgz'
+    parser = argparse.ArgumentParser(description='hf test', allow_abbrev=False)
+    group = parser.add_argument_group(title='test args')
+    group.add_argument('--pretrained-model-path', type=str, default="/home/ubuntu/cloudfs/saved_models/bigscience/bloomz-3b",
+                       help='Number of transformer layers.')
+    group.add_argument('--max-seq-length', type=int, default=1024)
+    args = parser.parse_args()
+
+    tokenizer = LlamaTokenizer.from_pretrained(
+            "decapoda-research/llama-7b-hf", add_eos_token=True
+        )
+
+    data = load_dataset("csv", data_files=datafile).train_test_split(
+            test_size=100, shuffle=True, seed=42
+        )
+    train_data = data["test"].shuffle().map(partial(generate_and_tokenize_prompt, tokenizer=tokenizer))
+
+    for i, batch in enumerate(train_data):
+        print(f"{i}: {batch}")
+
+    #print(testml[10])
+    #print(testml.tokenizer.decode(testml[10]['input_ids']))
+    #print(testml.tokenizer.decode(testml[23]['input_ids']))
+    #print(testml.tokenizer.decode(testml[17]['input_ids']))
+    #print(len(testml))
+    #print(f"max len:{testml.max_seq_length}")
+
+    for testing_type in ["newrank_healthcare", "zhihu_search", "baidubaijia"]:
+
+
+
+        for i in range(100, 40, -1):
+            print(f"\nfor max len: {i}")
+
+            res = partial(generate_and_tokenize_prompt, tokenizer=tokenizer, max_seq_length=i)({"data_type":"redbook_content_title",
+                                 "title_template_name":'制造悬念感',
+                                 "source_category":testing_type, 'content':'将数据转换成模型训练的输入将数据转换成模型训练的输入将数据转换成模型训练的输入', 'title':'将数据转换成模型训练的输入'})
+
+            print(f"encode len: {res['input_ids'].shape}")
+            deres = tokenizer.decode(res['input_ids'])
+            print(f"decoded encode: {deres}")
+
+
+    for title_len in range(80, 150):
+        title = ['数'] * title_len
+        title = ''.join(title)
+        print(f"title len:{title_len}")
+
+        res = partial(generate_and_tokenize_prompt, tokenizer=tokenizer, max_seq_length=200)({"data_type":"redbook_content_title",
+                             "source_category":"newrank_healthcare",
+                                 "title_template_name":'权威/内部人士/过来人身份推荐背书',
+                             'content':'将数据转换成模型训练的输入将数据转换成模型训练的输入将数据转换成模型训练的输入', 'title':title})
+
+        print(f"encode len: {res['input_ids'].shape}")
+        deres = tokenizer.decode(res['input_ids'])
+        print(f"decoded encode: {deres}")
+
+    from tqdm import tqdm
+
+    print(f"test iterate through all train data...")
+    data_path = "/home/ubuntu/cloudfs/ghost_data/merge_all_add_1208_1228//merge_all_0108_with_multichoice_scores_and_templates_train_1673194850.csv.tgz"
+
+    dataset = load_dataset("csv", data_files=data_path).shuffle().map(partial(generate_and_tokenize_prompt, tokenizer=tokenizer))
+
+
+
+    #dl = DataLoader(testml, batch_size=4, num_workers=32,pin_memory=False)
+    for x in tqdm(dataset, total=len(dataset)):
+        abc = x
+
+    #print(f"test iterate through all val data...")
+    #testml = GPT2QADataset(
+    #    "/home/ubuntu/cloudfs/ghost_data/merge_all_add_1208_1228//merge_all_0108_with_multichoice_scores_and_templates_val_1673194850.csv.tgz",
+    #    'medical_qa', args=args)
+
+    #dl = DataLoader(testml, batch_size=4, num_workers=32,pin_memory=False)
+    #for x in tqdm(testml, total=len(testml)):
+    #    abc = x

0941d02a081b9352c1c79a12e77d688e4d427bd9
Yu Li <lyo.gavin@gmail.com>
2023-04-07 00:12:13 -0500

    13b

--- finetune_tt_til0305_on0406.sh
+++ finetune_tt_til0305_on0406.sh
@@ -20,10 +20,12 @@ PORT="12345"
 
 DATA_PATH="/home/ubuntu/cloudfs/ghost_data/newred_redbook_link_download/api_0305_download/merge_all_till0305_with_multichoice_scores_and_templates_1679690410.csv.tgz" #"../dataset/instruction/guanaco_non_chat_mini_52K-utf8.json" #"./sample/merge_sample.json"
 
-MODEL_PATH="decapoda-research/llama-7b-hf"
+#MODEL_PATH="decapoda-research/llama-7b-hf"
+MODEL_PATH="decapoda-research/llama-13b-hf"
 
 # using checkpoint-final will cuase issue
-lora_checkpoint="/home/ubuntu/cloudfs/Chinese-Vicuna/lora-Vicuna/checkpoint-final"
+#lora_checkpoint="/home/ubuntu/cloudfs/Chinese-Vicuna/lora-Vicuna/checkpoint-final"
+lora_checkpoint="Chinese-Vicuna/Chinese-Vicuna-lora-13b-belle-and-guanaco"
 TEST_SIZE=0.05
 from_data_beginning=True # False
 

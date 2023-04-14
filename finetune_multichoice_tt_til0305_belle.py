9a897f269e3c2f91a6623974047553fbe865da2d
Yu Li <lyo.gavin@gmail.com>
2023-04-14 09:33:21 -0500

    use remote model

--- finetune_multichoice_tt_til0305_belle.py
+++ finetune_multichoice_tt_til0305_belle.py
@@ -12,12 +12,12 @@ import argparse
 import warnings
 from functools import partial
 
-from MultichoiceTemplateALpacaLORAWAYDataset import generate_and_tokenize_prompt, df_cols_to_use
+from generate_and_tokenize_prompt_belle import generate_and_tokenize_prompt, df_cols_to_use
 
 assert (
     "LlamaTokenizer" in transformers._import_structure["models.llama"]
 ), "LLaMA is now in HuggingFace's main branch.\nPlease reinstall it: pip uninstall transformers && pip install git+https://github.com/huggingface/transformers.git"
-from transformers import LlamaForCausalLM, LlamaTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig
 from peft import (
     prepare_model_for_int8_training,
     LoraConfig,
@@ -31,9 +31,9 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--wandb", action="store_true", default=False)
 parser.add_argument("--use_test", action="store_true", default=False)
 parser.add_argument("--data_path", type=str, default="merge.json")
-parser.add_argument("--output_path", type=str, default="lora-Vicuna")
-parser.add_argument("--model_path", type=str, default="decapoda-research/llama-7b-hf")
-parser.add_argument("--tokenizer_path", type=str, default="decapoda-research/llama-7b-hf")
+parser.add_argument("--output_path", type=str, default="belle_7b_2m")
+parser.add_argument("--model_path", type=str, default="BelleGroup/BELLE-7B-2M")
+parser.add_argument("--tokenizer_path", type=str, default="BelleGroup/BELLE-7B-2M")
 
 parser.add_argument("--eval_steps", type=int, default=200)
 parser.add_argument("--save_steps", type=int, default=200)
@@ -41,7 +41,7 @@ parser.add_argument("--run_ts", type=int)
 parser.add_argument("--test_size", type=float, default=0.05)
 parser.add_argument("--resume_from_checkpoint", type=str, default=None)
 parser.add_argument("--ignore_data_skip", type=str, default="False")
-parser.add_argument("--max_seq_len", type=int, default=700)
+parser.add_argument("--max_seq_len", type=int, default=1000)
 args = parser.parse_args()
 
 if args.use_test:
@@ -81,10 +81,10 @@ MICRO_BATCH_SIZE = 8  # this could actually be 5 but i like powers of 2
 BATCH_SIZE = 128
 MAX_STEPS = None
 GRADIENT_ACCUMULATION_STEPS = BATCH_SIZE // MICRO_BATCH_SIZE
-EPOCHS = 3  # we don't always need 3 tbh
+EPOCHS = 10  # we don't always need 3 tbh
 LEARNING_RATE = 8e-5  # the Karpathy constant
 CUTOFF_LEN = 256  # 256 accounts for about 96% of the data
-LORA_R = 8
+LORA_R = 16
 LORA_ALPHA = 16
 LORA_DROPOUT = 0.05
 VAL_SET_SIZE = args.test_size #2000
@@ -93,10 +93,7 @@ if VAL_SET_SIZE > 1.:
     VAL_SET_SIZE = int(VAL_SET_SIZE)
 
 
-TARGET_MODULES = [
-    "q_proj",
-    "v_proj",
-]
+TARGET_MODULES = ['query_key_value']
 DATA_PATH = args.data_path #"/home/cciip/private/fanchenghao/dataset/instruction/merge.json"
 OUTPUT_DIR = args.output_path #"lora-Vicuna"
 
@@ -107,16 +104,21 @@ if ddp:
     device_map = {"": int(os.environ.get("LOCAL_RANK") or 0)}
     GRADIENT_ACCUMULATION_STEPS = GRADIENT_ACCUMULATION_STEPS // world_size
 logger.info(args.model_path)
-model = LlamaForCausalLM.from_pretrained(
+model = AutoModelForCausalLM.from_pretrained(
     args.model_path,
     load_in_8bit=True,
     device_map=device_map,
+    cache_dir="/home/ubuntu/cloudfs/huggingfacecache/",
     proxies={
         'http':'socks5h://exmpl:abcd1234@43.156.235.42:8128',
         'https':'socks5h://exmpl:abcd1234@43.156.235.42:8128'}
 )
-tokenizer = LlamaTokenizer.from_pretrained(
-    args.tokenizer_path#, add_eos_token=True
+tokenizer = AutoTokenizer.from_pretrained(
+    args.tokenizer_path, #, add_eos_token=True
+    cache_dir="/home/ubuntu/cloudfs/huggingfacecache/",
+    proxies={
+        'http':'socks5h://exmpl:abcd1234@43.156.235.42:8128',
+        'https':'socks5h://exmpl:abcd1234@43.156.235.42:8128'}
 )
 
 model = prepare_model_for_int8_training(model)
@@ -153,7 +155,7 @@ if LOAD_PEFT_CHECKPOINT_FROM_PRETRAIN:
     logger.info(f"loaded checkpoint, setting to NONE: {args.resume_from_checkpoint}")
 
 tokenizer.pad_token_id = 0  # unk. we want this to be different from the eos token
-#tokenizer.padding_side = "left"  # Allow batched inference
+tokenizer.padding_side = "left"  # Allow batched inference
 
 #data = load_dataset("json", data_files=DATA_PATH)
 
@@ -296,7 +298,7 @@ class MyCallback(transformers.TrainerCallback):
             input_ids = input_ids.to('cuda')
             generation_output = model.generate(
                 input_ids=input_ids,
-                max_new_tokens=15,
+                max_new_tokens=35,
             )
             #print(generation_output)
             logger.info(tokenizer.decode(generation_output[0]))
@@ -315,9 +317,25 @@ class MyCallback(transformers.TrainerCallback):
             model = kwargs['model']
             input_ids = tokenizer(inputs, return_tensors="pt")['input_ids']
             input_ids = input_ids.to('cuda')
+
+            generation_config = GenerationConfig(
+                temperature=1.,
+                top_p=0.9,
+                top_k=3,
+                num_beams=4,
+                bos_token_id=1,
+                eos_token_id=2,
+                pad_token_id=0,
+                max_new_tokens=35,  # max_length=max_new_tokens+input_sequence
+                min_new_tokens=10,  # min_length=min_new_tokens+input_sequence
+                #num_return_sequences=2,
+                #**kwargs,
+            )
+
             generation_output = model.generate(
                 input_ids=input_ids,
-                max_new_tokens=25,
+                generation_config=generation_config,
+                max_new_tokens=35,
             )
             #print(generation_output)
             logger.info(tokenizer.decode(generation_output[0]))
--- finetune_tt_til0305_belle_on0413.sh
+++ finetune_tt_til0305_belle_on0413.sh
@@ -14,24 +14,26 @@ OUTPUT_PATH=$ROOT_DIR_BASE/output_$run_ts
 mkdir -p $OUTPUT_PATH
 
 
-DATA_PATH="/home/ubuntu/cloudfs/ghost_data/newred_redbook_link_download/api_0305_download/merge_all_till0305_with_multichoice_scores_and_templates_train_1679690410.csv.tgz" #"../dataset/instruction/guanaco_non_chat_mini_52K-utf8.json" #"./sample/merge_sample.json"
+DATA_PATH="/home/ubuntu/cloudfs/ghost_data/newred_redbook_link_download/api_0305_download/" \
+  "merge_all_till0305_with_multichoice_scores_and_templates_val_hot_notes_only_1679690410.csv.tgz" #"../dataset/instruction/guanaco_non_chat_mini_52K-utf8.json" #"./sample/merge_sample.json"
 
 #MODEL_PATH="decapoda-research/llama-7b-hf"
-MODEL_PATH="decapoda-research/llama-13b-hf"
+MODEL_PATH="/home/ubuntu/cloudfs/saved_models/BelleGroup/BELLE-7B-2M/"
 
 # use local model
 #MODEL_PATH="/home/ubuntu/cloudfs/saved_models/decapoda-research/llama-13b-hf"
-TOKENIZER_PATH="decapoda-research/llama-13b-hf"
+TOKENIZER_PATH="BelleGroup/BELLE-7B-2M"
 # using checkpoint-final will cuase issue
 #lora_checkpoint="/home/ubuntu/cloudfs/Chinese-Vicuna/lora-Vicuna/checkpoint-final"
 #lora_checkpoint="Chinese-Vicuna/Chinese-Vicuna-lora-13b-belle-and-guanaco"
-lora_checkpoint="/home/ubuntu/cloudfs/Chinese-Vicuna/lora-Vicuna/Chinese-Vicuna-lora-13b-belle-and-guanaco"
+#lora_checkpoint="/home/ubuntu/cloudfs/Chinese-Vicuna/lora-Vicuna/Chinese-Vicuna-lora-13b-belle-and-guanaco"
 TEST_SIZE=5
 from_data_beginning=True # False
 
 #--use_test \
+#--resume_from_checkpoint $lora_checkpoint \
 
-CUDA_VISIBLE_DEVICES="0" nohup python finetune_multichoice_tt_til0305.py \
+CUDA_VISIBLE_DEVICES="0" nohup python finetune_multichoice_tt_til0305_belle.py \
 --data_path $DATA_PATH \
 --output_path $OUTPUT_PATH \
 --model_path $MODEL_PATH \
@@ -39,9 +41,9 @@ CUDA_VISIBLE_DEVICES="0" nohup python finetune_multichoice_tt_til0305.py \
 --eval_steps 5 \
 --save_steps 5 \
 --test_size $TEST_SIZE \
---resume_from_checkpoint $lora_checkpoint \
 --run_ts $run_ts \
---max_seq_len 700 \
+--max_seq_len 1000 \
+--use_test \
 --wandb \
 --ignore_data_skip $from_data_beginning &
 

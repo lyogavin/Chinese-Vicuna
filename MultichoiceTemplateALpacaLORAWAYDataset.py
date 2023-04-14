89a70bd4140406a46d9ad4b4c4718c2d8cc83b6c
Yu Li <lyo.gavin@gmail.com>
2023-04-13 20:19:32 -0500

    fix tokenize parameter

--- MultichoiceTemplateALpacaLORAWAYDataset.py
+++ MultichoiceTemplateALpacaLORAWAYDataset.py
@@ -108,7 +108,8 @@ if __name__ == '__main__':
     tokenizer.pad_token_id = 0
 
     df = pd.read_csv(data_file)[df_cols_to_use]
-    print(f"{df.dtypes}")
+    print(f"df types: {df.dtypes}")
+    print(f"df head: {df.head()}")
 
     data = Dataset.from_pandas(df).train_test_split(
             test_size=100, shuffle=True, seed=42

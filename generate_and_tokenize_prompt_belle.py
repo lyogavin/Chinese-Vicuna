724a81370a20ac2bfc4f0d68176efc8aed42e1c1
Yu Li <lyo.gavin@gmail.com>
2023-04-14 00:25:42 -0500

    fix target

--- generate_and_tokenize_prompt_belle.py
+++ generate_and_tokenize_prompt_belle.py
@@ -276,10 +276,10 @@ if __name__ == '__main__':
         elif test_title not in deres and test_content in deres:
             assert False
 
-        assert deres.startswith(tokenizer.bos_token)
-        assert deres.endswith(tokenizer.eos_token)
-        assert tokenizer.bos_token not in deres[1:]
-        assert tokenizer.eos_token not in deres[:-1]
+        assert res['input_ids'][0] == tokenizer.bos_token_id, f"deres:{deres}"
+        assert res['input_ids'][-1] == tokenizer.eos_token_id
+        assert tokenizer.bos_token_id not in deres[1:]
+        assert tokenizer.eos_token_id not in deres[:-1]
 
 
 

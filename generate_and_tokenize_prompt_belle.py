c8cc80ae40176fe1565fa3471cd40aa08804ceee
Yu Li <lyo.gavin@gmail.com>
2023-04-13 20:21:10 -0500

    fix tokenize parameter

--- generate_and_tokenize_prompt_belle.py
+++ generate_and_tokenize_prompt_belle.py
@@ -143,8 +143,10 @@ if __name__ == '__main__':
     tokenizer.pad_token_id = 0
     tokenizer.padding_side = "left"
 
-    df = pd.read_csv(data_file)
 
+    df = pd.read_csv(data_file)[df_cols_to_use]
+    print(f"df types: {df.dtypes}")
+    print(f"df head: {df.head()}")
     data = Dataset.from_pandas(df).train_test_split(
             test_size=100, shuffle=True, seed=42
         )

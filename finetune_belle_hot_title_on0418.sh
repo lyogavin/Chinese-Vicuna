c7a917721a94787c1a031f5751217617c2ac0c20
Yu Li <lyo.gavin@gmail.com>
2023-04-19 14:31:43 -0500

    formal train

--- finetune_belle_hot_title_on0418.sh
+++ finetune_belle_hot_title_on0418.sh
@@ -44,6 +44,5 @@ CUDA_VISIBLE_DEVICES="0" nohup python finetune_belle_hot_title_0418.py \
 --run_ts $run_ts \
 --max_seq_len 120 \
 --wandb \
---use_test \
 --ignore_data_skip $from_data_beginning &
 

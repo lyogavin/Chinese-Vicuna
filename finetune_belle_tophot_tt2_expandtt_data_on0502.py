34d8875afa7c727d2bfae48872aea1fe06b222dc
Yu Li <lyo.gavin@gmail.com>
2023-05-02 18:00:37 -0500

    smaller cut

--- finetune_belle_tophot_tt2_expandtt_data_on0502.py
+++ finetune_belle_tophot_tt2_expandtt_data_on0502.py
@@ -84,12 +84,12 @@ USE_TEST = args.use_test
 
 
 # optimized for RTX 4090. for larger GPUs, increase some of these?
-MICRO_BATCH_SIZE = 8  # this could actually be 5 but i like powers of 2
+MICRO_BATCH_SIZE = 4  # this could actually be 5 but i like powers of 2
 BATCH_SIZE = 128
 MAX_STEPS = None
 GRADIENT_ACCUMULATION_STEPS = BATCH_SIZE // MICRO_BATCH_SIZE
 EPOCHS = 3  # we don't always need 3 tbh
-LEARNING_RATE = 3e-4  # the Karpathy constant
+LEARNING_RATE = 2e-4  # the Karpathy constant
 CUTOFF_LEN = 256  # 256 accounts for about 96% of the data
 LORA_R = 512
 LORA_ALPHA = 16
